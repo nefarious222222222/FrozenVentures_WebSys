0ca3107e2d18badd6ad270818c579ef6ae07f3ec
chowa <joschuaordonio29@gmail.com>
2024-04-23 21:48:45 +0800

    server signin

--- server/signin.php
+++ server/signin.php
@@ -1,10 +1,21 @@
 <?php
 
-include 'dbConnection.php';
-
 //start session
 session_start();
 
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if($username == "Admin" && $password == "Admin123"){
+        $_SESSION['valid'] = $username;
+        header("location: ../pages/processes/billing.php");
+    }
+    else{
+        echo "Wrong password";
+    }
+
+}
 
 ?>
