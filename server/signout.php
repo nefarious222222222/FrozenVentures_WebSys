29013c986079d26adc3cc1f4deba41d8a95de318
nirapyus <vjpcanaria@bpsu.edu.ph>
2024-04-29 12:40:30 +0800

    push

--- server/signout.php
+++ server/signout.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-// Destroy the session
+
 session_destroy();
 
-// Redirect to the login page or any other desired page
-header("Location: ../index.php");
+echo "<script>alert('Successfully Signed Out!');</script>";
+echo "<script>setTimeout(function() { window.location.href = ' ../pages/sign/sign-in.php'; }, 100);</script>";
 exit();
 ?>
